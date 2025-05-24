1c37819a282311dfad34e1a9d38e0b489908c0f4
Debasish Pothal <debasish_pothal@hotmail.com>
2025-05-24 08:31:46 +0530

    Create 274.H-Index.js

---
+++ 274.H-Index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) {
+  citations.sort((a, b) => a - b);
+  const n = citations.length;
+
+  for (let i = 0; i < n; i++) {
+    if (citations[i] >= n - i) {
+      return n - i;
+    }
+  }
+
+  return 0;
+};
