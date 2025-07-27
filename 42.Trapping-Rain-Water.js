fe545b534032192e39896b347c73584cf22cae16
Debasish Pothal <debasish_pothal@hotmail.com>
2025-07-27 10:13:01 +0530

    Update 42.Trapping-Rain-Water.js

--- 42.Trapping-Rain-Water.js
+++ 42.Trapping-Rain-Water.js
@@ -3,33 +3,23 @@
  * @return {number}
  */
 var trap = function (height) {
-  // min(L, R) - height[i];
-
-  const length = height.length;
-  const maxLeft = new Array(length);
-  const maxRight = new Array(length);
-
-  maxLeft[0] = height[0];
-  maxRight[length - 1] = height[length - 1];
-
-  const minLR = new Array(length);
-
-  for (let i = 1; i < length; i++) {
-    maxLeft[i] = Math.max(maxLeft[i - 1], height[i]);
-  }
-
-  for (let i = length - 2; i >= 0; i--) {
-    maxRight[i] = Math.max(maxRight[i + 1], height[i]);
-  }
-
-  for (let i = 0; i < length; i++) {
-    minLR[i] = Math.min(maxLeft[i], maxRight[i]);
-  }
+  let left = 0;
+  let right = height.length - 1;
+  let leftMax = height[left];
+  let rightMax = height[right];
 
   let water = 0;
 
-  for (let i = 0; i < length; i++) {
-    water += Math.abs(minLR[i] - height[i]);
+  while (left < right) {
+    if (leftMax < rightMax) {
+      left += 1;
+      leftMax = Math.max(leftMax, height[left]);
+      water += leftMax - height[left];
+    } else {
+      right -= 1;
+      rightMax = Math.max(rightMax, height[right]);
+      water += rightMax - height[right];
+    }
   }
 
   return water;
